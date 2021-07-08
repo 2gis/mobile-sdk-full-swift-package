ac58d49748cad7ed9573951483399d07875cb34c
Sergey Lagner <s.lagner@2gis.ru>
2021-07-08 21:24:19 +0700

    Version 1.0.1-rc.1

--- Package.swift
+++ Package.swift
@@ -1,21 +1,20 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
   name: "DGisMobileSDK",
   products: [
     .library(
-      name: "FullSDK",
+      name: "MapSDK",
       targets: ["DGis"])
   ],
   targets: [
     .binaryTarget(
       name: "DGis",
       url:
-        "https://maven.2gis.dev/sdk-ios-rc/sdk-release-v1.0.0/Release/full-RC-1625158954969.xcframework.zip",
-      checksum: "e90c84912ce258307c3772b25ad2e75303f26f44430d0ba4ceecd16948358b43"
+        "https://artifactory.2gis.dev/sdk-ios-rc/sdk-release-v1.0.1/Release/full-RC-1625727192813.xcframework.zip",
+      checksum: "7f701a8d141bf17ae7cb9502ca4d2a1228f926e8cb8566a7130fa092044e98d6"
     )
   ]
 )
