71e8179c4b1d2e966cecc9c78c07e03337b6f1a7
buildserver-v4core <uk.github.auth@2gis.ru>
2022-07-15 17:50:30 +0700

    Bump version to 4.1.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     .binaryTarget(
       name: "DGis",
       url:
-        "https://artifactory.2gis.dev/sdk-ios-release/2.8.1/Release/DGisFullSDK.zip",
-      checksum: "0ddae150a94f07292c1669901d4f062433b7a1c387ca14876fae7d69a4f758f1"
+        "https://artifactory.2gis.dev/sdk-ios-release/4.1.0/Release/DGisFullSDK.zip",
+      checksum: "ed54fc5310f9095f0deae1279e2a21b91d608ebbe9ed01210fd21007dbb289d3"
     )
   ]
 )
